36c21ce439328941377ab3f7cb5d180eb849e5b3
test-sy <65141428+test-sy@users.noreply.github.com>
2020-05-22 15:30:41 +0900

    5/22作業

--- みにげーむ/Assets/script/PlayerScore.cs
+++ みにげーむ/Assets/script/PlayerScore.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Action1Main : MonoBehaviour
+public class PlayerScore : MonoBehaviour
 {
-    public Transform charcter;
-    public Text scoreLabel;
-
     // Start is called before the first frame update
     void Start()
     {
-        int score = CalcScore();
-        scoreLabel.text = "Score:" + score + "m";
+        
     }
 
     // Update is called once per frame
